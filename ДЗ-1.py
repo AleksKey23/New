f565b7585ba7fc234abbe7ad9f5b61a06acec966
AleksKey23 <123273754+AleksKey23@users.noreply.github.com>
2023-09-05 22:47:56 +0300

    Add files via upload

---
+++ ДЗ-1.py
@@ -0,0 +1,28 @@
+
+# Первое задание
+
+print('Vid pitomza: ')
+vid=input ()
+print('Vozrast pitomza: ')
+age=input ()
+print('Klichka pitomza: ')
+name=input ()
+print('U vas ',vid, ', vozrast ', age, ', klichka ', name)
+
+
+
+# Второе задание
+print()
+print('''Vvedite 7 etapov stanovleniya cheloveka po poryadku. 
+P.S. Nachinaetsya s Australopithecus afarensis:''')
+a= input ()
+b= input ()
+c= input ()
+d= input ()
+e= input ()
+f= input ()
+g= input ()
+print ('Vash otven:')
+print (a,b,c,d,e,f,g, sep=' => ')
+print ('''Verniy otvet takoy:
+  Australopithecus afarensis => Australopithecus africanus => Homo habilis => Homo erectus => Homo sapiens neanderthalensis => Homo sapiens linnaeus => Homo sapiens sapiens''')
