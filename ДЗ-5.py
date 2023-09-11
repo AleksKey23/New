f7a03aeb111d2268c7c0ae0a0bdae1d0c36850a0
AleksKey23 <123273754+AleksKey23@users.noreply.github.com>
2023-09-11 21:45:28 +0300

    Add files via upload

---
+++ ДЗ-5.py
@@ -0,0 +1,20 @@
+﻿
+
+# Test 1
+
+sl = input(' Введите слово: ')
+resl = sl[::-1]
+if resl==sl:
+    print (' Это палиндром.')
+else:
+    print (' Это не палиндром.')
+    
+
+
+# Test 2 
+
+print ('\n\n Введите строку для редактирования: ')
+stroka = input()
+sbp = stroka.split()
+print (' '.join(sbp))
+
